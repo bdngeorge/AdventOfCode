3380b5e41c142512695c663c5335e2d5b999205d
Brandon George <bgeorge@cfse.biz>
2023-12-05 08:21:26 -0500

    day 4 part 2

--- AOC2023/Day4/part2.py
+++ AOC2023/Day4/part2.py
@@ -1,9 +1,10 @@
 import re
 
 if __name__ == "__main__":
-  input = open("inputSmall.txt", "r").read().strip().split("\n")
+  input = open("input.txt", "r").read().strip().split("\n")
 
   total = 0
+  copies = {}
   for game in input:
     card, numbers = game.strip().split(":")
 
@@ -12,17 +13,31 @@ if __name__ == "__main__":
 
     winningNums = re.findall(r'\d+', winningNumsStr)
     elfsNums = re.findall(r'\d+', elfsNumsStr)
-    cardNum = card.split(" ")[1]
-    print(cardNum)
+    cardNum = (int)(card.split(" ")[-1])
+    if cardNum not in copies.keys():
+      copies[cardNum] = 1
 
     subtotal = 0
+    matches = 0
     for number in elfsNums:
       if number in winningNums:
         if subtotal == 0:
           subtotal += 1
         else:
           subtotal *= 2
+        matches += 1
+    
+    if matches > 0:
+      for i in range(cardNum+1, matches+cardNum+1):
+        if i not in copies.keys():
+          copies[i] = 1
+        copies[i] += copies[cardNum]
     
     total += subtotal
 
+  totalCopies = 0
+  for key in copies:
+    totalCopies += copies[key]
+  
+  print(totalCopies)
   print(total)
