9ca8fc760b049ac2b59ae802a96c6499cd5ee4aa
Brandon George <bgeorge@cfse.biz>
2023-12-05 16:02:56 -0500

    start part 2

---
+++ AOC2023/Day5/part2.py
@@ -0,0 +1,49 @@
+# Find the 'goal' in the current 'index' and recurse down to find the location
+# goal = current number (start at seed)
+# index = current map (start at 0, seed to soil)
+def FindLocation(current, index, maps):
+    if index >= len(maps):
+        return current
+
+    map = maps[index]
+
+    next = -1
+    for numbers in map[1:]:
+        dest, source, range = numbers.split(" ")
+        if (int)(source) <= current and (int)(source) + (int)(range) - 1 >= current:
+            next = (int)(dest) + current - (int)(source)
+            break
+    
+    if next == -1:
+        next = current
+    
+    return FindLocation(next, index + 1, maps)
+
+if __name__ == "__main__":
+    input = open("input.txt", "r").read().split("\n")
+
+    seedRanges = input[0].strip().split(" ")[1:]
+    seeds = []
+    i = 0
+    while i < len(seedRanges):
+        for j in range(i, (int)(seedRanges[i+1])):
+            seeds.append((int)(seedRanges[i]) + j)
+        i += 2
+
+    #Store the maps in sublists
+    maps = []
+    sublist = []
+    for i in range(2, len(input)):
+        if input[i] == "":
+            maps.append(sublist)
+            sublist = []
+        else:
+            sublist.append(input[i])
+
+    locations = []
+    for seed in seeds:
+        # Run down the maps to find the location
+        locations.append(FindLocation((int)(seed), 0, maps))
+    
+    finalLocation = min(locations)
+    print(finalLocation)
