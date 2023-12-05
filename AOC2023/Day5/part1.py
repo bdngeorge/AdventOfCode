78d00eb072818ad21f98ebf4055254eaa7e798ac
Brandon George <bgeorge@cfse.biz>
2023-12-05 14:59:32 -0500

    day 5 part 1

---
+++ AOC2023/Day5/part1.py
@@ -0,0 +1,43 @@
+# Find the 'goal' in the current 'index' and recurse down to find the location
+# goal = current number (start at seed)
+# index = current map (start at 0, seed to soil)
+def FindLocation(current, index, maps):
+    if index >= len(maps):
+        return current
+
+    map = maps[index]
+
+    next = -1
+    for numbers in map[1:]:
+        dest, source, range = numbers.split(" ")
+        if (int)(source) <= current and (int)(source) + (int)(range) - 1 >= current:
+            next = (int)(dest) + current - (int)(source)
+            break
+    
+    if next == -1:
+        next = current
+    
+    return FindLocation(next, index + 1, maps)
+
+if __name__ == "__main__":
+    input = open("input.txt", "r").read().split("\n")
+
+    seeds = input[0].strip().split(" ")[1:]
+
+    #Store the maps in sublists
+    maps = []
+    sublist = []
+    for i in range(2, len(input)):
+        if input[i] == "":
+            maps.append(sublist)
+            sublist = []
+        else:
+            sublist.append(input[i])
+
+    locations = []
+    for seed in seeds:
+        # Run down the maps to find the location
+        locations.append(FindLocation((int)(seed), 0, maps))
+    
+    finalLocation = min(locations)
+    print(finalLocation)
